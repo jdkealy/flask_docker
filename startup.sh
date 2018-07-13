481c40474a17a5fc850a3187fc9250dd9a603fbb
jdkealy <jdkealy@gmail.com>
2018-07-13 13:23:26 -0400

    startup script

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
-export EXTERNAL_IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}') 
+export DISPLAY=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+export IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 docker-compose up 
